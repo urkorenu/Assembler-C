d310014b4d053ef3adc20c78a1372748c16948b8
yamsh1 <161297147+yamsh1@users.noreply.github.com>
2024-04-15 11:29:45 +0300

    Update encode.h

--- src/encode.h
+++ src/encode.h
@@ -6,6 +6,23 @@
 #include "linked_list.h"
 #include "parser.h"
 
+/*The file contain all functions for encoding bits in the program*/
+
+/**
+ * @brief This function adds bits to a number in a certain position
+ *
+ * The function takes integer type number, which is the number to which we want to add bits, 
+   integer type number the bits we want to add, and integer type nuber the position where we want to add the bits
+
+ * The function performs one iteration unless requested otherwise
+ 
+ 
+ * @param source: An integer type number to which we want to add bits
+ * @param data: An integer type number that represents the number we want to add
+ * @param location: A variable of type int that symbolizes the position where we want to add the bits
+ 
+ * @return An extern integer indicatingthe the number after adding the bits.
+ */
 extern int
 add_bits(int source, int data, int location);
 
