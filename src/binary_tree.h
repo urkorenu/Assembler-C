7dc3862dbb9defed88b0414810b77d43640bcc88
yamsh1 <161297147+yamsh1@users.noreply.github.com>
2024-04-21 12:00:50 +0300

    Update binary_tree.h

--- src/binary_tree.h
+++ src/binary_tree.h
@@ -29,9 +29,21 @@ struct binary_tree {
 struct tnode
 tnode_init(void);
 
+/**
+ * @brief This function finds the previous node of a given key in a binary tree.
+ * @param root The root node of the binary tree.
+ * @param key The key to find the previous node for.
+ * @return Returns a pointer to the previous node of the given key if found, otherwise returns NULL.
+ */  
 extern struct tnode*
 find_previous_node(struct tnode* root, const char* key);
 
+/**
+ * @brief This function retrieves data associated with the given key from a binary tree.
+ * @param tree The binary tree containing the data.
+ * @param key The key to search for in the binary tree.
+ * @return Returns a pointer to the data associated with the given key if found, otherwise returns NULL.
+ */ 
 extern void*
 get_data_by_key(struct binary_tree* tree, const char* key);
 
